ee20c6a3185a52576a2f3b4dc5a0eadabdcb5626
cesaral <cesar.alejandro.ibarra.roque@intel.com>
2018-10-23 22:07:49 -0500

    New conexion

--- AnimalesEnPeligro/Conexion.cs
+++ AnimalesEnPeligro/Conexion.cs
@@ -11,6 +11,35 @@ namespace AnimalesEnPeligro
 {
     class Conexion
     {
-        public static SqlConnection conn = new SqlConnection("Initial Catalog=Proyectos; Data Source=localhost; Integrated Security=SSPI;");
+        public static SqlConnection conn = new SqlConnection("Initial Catalog=examen; Data Source=localhost; Integrated Security=SSPI;");
+
+        public DataSet ConsultaTab(string tabla, string campo)
+        {
+            DataSet datSet = new DataSet();
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+            String consulta = string.Format("Select * from {0} order by {1} ASC", tabla, campo);
+            SqlCommand comando = new SqlCommand(consulta, conn);
+            adaptador.SelectCommand = comando;
+            conn.Open();
+            adaptador.Fill(datSet, tabla);
+            conn.Close();
+            return datSet;
+
+        }
+
+        public int ABM(string instruccion)
+        {
+            int respuesta = -1;
+            SqlCommand comando = new SqlCommand(instruccion, conn);
+            conn.Open();
+            respuesta = comando.ExecuteNonQuery();
+            conn.Close();
+            return respuesta;
+        }
+
+
+
+
     }
+
 }
--- AnimalesEnPeligro/especies.cs
+++ AnimalesEnPeligro/especies.cs
@@ -1,19 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AnimalesEnPeligro
 {
     class especies
     {
+        Conexion BD = new Conexion();
+        int res = -1;
+
         public int idEspecie { get; set; }
         public string nombreCientifico { get; set; }
         public string nombreVulgar { get; set; }
         public string descripcion { get; set; }
         public string genero { get; set; }
         public byte img { get; set; }
+        public byte estatus { get; set; }
 
-}
+
+        void deleteEspecie(int idEspecie)
+        {
+            try
+            {
+                string eliminar = string.Format("DELETE FROM especies WHERE idEspecie={0}", idEspecie.ToString() );
+
+                res = BD.ABM(eliminar);
+
+                if (res == 1)
+                {
+                    MessageBox.Show("Se ha eliminado registro con codigo " + idEspecie.ToString(), "Bajas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexion.conn.Close();
+            }
+        }
+
+
+        private void MuestraDataEspecie(DataGridView dgv)
+        {
+            DataSet datos = new DataSet();
+            datos = BD.ConsultaTab("especies", "idEspecie");
+            dgv.DataSource = datos.Tables["especies"];
+           
+        }
+
+        private void registrarEspecie()
+        {
+            try
+            {
+                string insertar = string.Format("INSERT INTO especies VALUES( null, '{0}', '{1}', '{2}', '{3}', '{4}', '{5}')", this.nombreCientifico, this.nombreVulgar,
+                    this.descripcion, this.genero, this.img, this.estatus);
+
+                res = BD.ABM(insertar);
+
+                if (res == 1)
+                {
+                    MessageBox.Show("Se ha agregado un registro", "Alta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexion.conn.Close();
+            }
+
+        }
+
+
+        private void modificarEspecie()
+        {
+            try
+            {
+                string modificar = string.Format("UPDATE especies SET nombreCientifico='{0}', nombreVulgar='{1}', descripcion='{2}', " +
+                     "genero='{3}', img='{4}', estatus='{5}', WHERE idEspecie = {6}", this.nombreCientifico, this.nombreVulgar, 
+                     this.descripcion, this.genero, this.img, this.estatus, this.idEspecie);
+
+                res = BD.ABM(modificar);
+
+                if (res == 1)
+                {
+                    MessageBox.Show("Se ha modificado el registro correctamente", "Modificación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexion.conn.Close();
+            }
+        }
+
+
+
+
+    }
+
+
 }
